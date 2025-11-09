0da65928eb71c43c438090dac22083d5a9036605
Parastoo Majidi <61692804+parastoomajidi@users.noreply.github.com>
2025-11-09 06:44:22 +0000

    worked on groupBy-Like-select-limit and where with sqLite

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Daily--xercise
+![alt text](image.png)
--- src/fav.py
+++ src/fav.py
@@ -9,17 +9,17 @@ with open(csv_path, "r", encoding="utf-8") as file:
     counts = {}
 
     for row in reader:
-        fav2 = row["language"].strip()
-        if fav2 in counts:
-            counts[fav2] += 1
+        fav = row["language"].strip()
+        if fav in counts:
+            counts[fav] += 1
         else:
-            counts[fav2] = 1
+            counts[fav] = 1
 
 # for fav2, count in counts.items():
 #     print(f"{fav2}: {count}")
 
 # find the popular language
-for fav2 in sorted(counts, key=counts.get, reverse=True):
-    print(f"{fav2}: {counts[fav2]}")
+for fav in sorted(counts, key=counts.get, reverse=True):
+    print(f"{fav}: {counts[fav]}")
 
 
