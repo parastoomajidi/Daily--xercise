784ad17421837e7ed7986cddd78d614a89d12d72
Parastoo Majidi <61692804+parastoomajidi@users.noreply.github.com>
2025-11-09 05:56:56 +0000

    find must fav lang in prigraming

--- src/fav.py
+++ src/fav.py
@@ -15,5 +15,11 @@ with open(csv_path, "r", encoding="utf-8") as file:
         else:
             counts[fav2] = 1
 
-for fav2, count in counts.items():
-    print(f"{fav2}: {count}")
+# for fav2, count in counts.items():
+#     print(f"{fav2}: {count}")
+
+# find the popular language
+for fav2 in sorted(counts, key=counts.get, reverse=True):
+    print(f"{fav2}: {counts[fav2]}")
+
+
